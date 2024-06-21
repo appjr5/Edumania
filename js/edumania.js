e5d2f85a2b5307be470a778c95c759339e1737ca
appjr5 <appjr360@gmail.com>
2024-06-21 13:31:14 +0300

    first commit

---
+++ js/edumania.js
@@ -0,0 +1,72 @@
+// const courses = [
+//     {
+//         url: "#"
+//         title: "Math"
+//         thumbnail: ""
+//         category: ""
+//         description: ""
+//         price:""
+//         duration:""
+//     },
+//     {
+//         url: "#"
+//         title: "Math"
+//         thumbnail: ""
+//         category: ""
+//         description: ""
+//         price:""
+//         duration:""
+//     },
+//     {
+//         url: "#"
+//         title: "Math"
+//         thumbnail: ""
+//         category: ""
+//         description: ""
+//         price:""
+//         duration:""
+//     },
+//     {
+//         url: "#"
+//         title: "Math"
+//         thumbnail: ""
+//         category: ""
+//         description: ""
+//         price:""
+//         duration:""
+//     },
+// ]
+// const displayCourse = (urlValue, 
+// titleValue, 
+// categoryValue, 
+// descriptionValue, 
+// thumbnailValue, 
+// priceValue, 
+// durationValue)
+// {
+//     const card = document.createElementt("div");
+//     card.classList.add("card");
+
+//     const a = document.createElementt("a");
+//     a.setAttribute("href", urlValue);
+
+//     const category = document.createElementt("div");
+//     category.classList.add("category");
+//     category.innerHTML = categoryValue;
+
+//     const img = document.createElementt("img");
+//     img.setAttribute("src", thumbnailValue);
+
+//     const title = document.createElementt("h2");
+//     title.classList.add("title");
+//     title.innerHTML = titleValue;
+
+//     const description = document.createElementt("div");
+//     description.classList.add("description");
+//     description.innerHTML = descriptionValue;
+
+//     const info = document.createElementt("div");
+//     info.classList.add("info");
+
+    
+// }
